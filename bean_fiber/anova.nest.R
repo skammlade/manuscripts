7f86f94eed44ec69b26820cfa68a2c20370f14e4
skammlade <sara.kammlade@gmail.com>
2016-06-22 15:53:04 -0600

    fibersub fibertype~Rep + race/mktclass

--- bean_fiber/anova.nest.R
+++ bean_fiber/anova.nest.R
@@ -12,16 +12,16 @@ fibersub <- subset(fiber,
                      mktclass=='black' | 
                      mktclass=='small white')
 
-OneWayFit.IDF <- aov(IDF~race/mktclass + Rep, data=fibersub)
+OneWayFit.IDF <- summary(aov(IDF~Rep + (race/mktclass), data=fibersub))
 
-OneWayFit.SDF <- aov(SDF~race/mktclass + Rep, data=fibersub)
+OneWayFit.SDF <- summary(aov(SDF~Rep + (race/mktclass), data=fibersub))
 
-OneWayFit.Raff <- aov(Raff~race/mktclass + Rep, data=fibersub)
+OneWayFit.Raff <- summary(aov(Raff~Rep + (race/mktclass), data=fibersub))
 
-OneWayFit.Stach <- aov(Stach~race/mktclass + Rep, data=fibersub)
+OneWayFit.Stach <- summary(aov(Stach~Rep + (race/mktclass), data=fibersub))
 
-OneWayFit.Verb <- aov(Verb~race/mktclass + Rep, data=fibersub)
+OneWayFit.Verb <- summary(aov(Verb~Rep + (race/mktclass), data=fibersub))
 
-OneWayFit.TOligos <- aov(TOligos~race/mktclass + Rep, data=fibersub)
+OneWayFit.TOligos <- summary(aov(TOligos~Rep + (race/mktclass), data=fibersub))
 
-OneWayFit.TDF <- aov(TDF~race/mktclass + Rep, data=fibersub)
+OneWayFit.TDF <- summary(aov(TDF~Rep + (race/mktclass), data=fibersub))
