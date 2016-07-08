33d4a4ee5c9ddc02125d17637ee0bad57311fa6e
skammlade <sara.kammlade@gmail.com>
2016-07-08 11:42:43 -0500

    remove error bars, y axis label

--- bean_fiber/lineplot.gxe.R
+++ bean_fiber/lineplot.gxe.R
@@ -17,9 +17,9 @@ summ.df <- summarySE(gxe.fibersub.TDF, measurevar="value", groupvars=c("Entry","
 #oligosaccharides by mktclass
 p.gxe.TDF <- ggplot(summ.df, aes(x=Origin, y=value, colour=Entry))
 
-p.gxe.TDF <- p.gxe.TDF  + geom_line(aes(group=Entry)) + geom_point() + geom_errorbar(aes(ymin=value-se, ymax=value+se), width=.1)
+p.gxe.TDF <- p.gxe.TDF  + geom_line(aes(group=Entry)) + geom_point() 
 
-p.gxe.TDF <- p.gxe.TDF + theme(axis.title.y=element_blank(),
+p.gxe.TDF <- p.gxe.TDF + theme(axis.title.y=element_text(size=12),
                                axis.text.y=element_text(family="Helvetica",
                                                         size=12),
                                axis.title.x=element_blank(),
@@ -28,7 +28,9 @@ p.gxe.TDF <- p.gxe.TDF + theme(axis.title.y=element_blank(),
                                                          size=12),
                                plot.margin=unit(c(0.5,0.5,0.5,0.5), "cm"), legend.position="none")
 
-
+p.gxe.TDF <- p.gxe.TDF + labs(y=NULL, 
+                               x=NULL, 
+                               title="Total Dietary Fiber")
 
 
 #subset fiber data to remove black mottle, flor de mayo, red mottle, carioca, and tan
@@ -41,9 +43,9 @@ summ.df <- summarySE(gxe.fibersub.IDF, measurevar="value", groupvars=c("Entry","
 #oligosaccharides by mktclass
 p.gxe.IDF <- ggplot(summ.df, aes(x=Origin, y=value, colour=Entry))
 
-p.gxe.IDF <- p.gxe.IDF  + geom_line(aes(group=Entry)) + geom_point() + geom_errorbar(aes(ymin=value-se, ymax=value+se), width=.1)
+p.gxe.IDF <- p.gxe.IDF  + geom_line(aes(group=Entry)) + geom_point() 
 
-p.gxe.IDF <- p.gxe.IDF + theme(axis.title.y=element_blank(),
+p.gxe.IDF <- p.gxe.IDF + theme(axis.title.y=element_text(size=12),
                                axis.text.y=element_text(family="Helvetica",
                                                         size=12),
                                axis.title.x=element_blank(),
@@ -52,7 +54,9 @@ p.gxe.IDF <- p.gxe.IDF + theme(axis.title.y=element_blank(),
                                                          size=12),
                                plot.margin=unit(c(0.5,0.5,0.5,0.5), "cm"), legend.position="none")
 
-
+p.gxe.IDF <- p.gxe.IDF + labs(y=NULL, 
+                              x=NULL, 
+                              title="Insoluble Dietary Fiber")
 
 
 
@@ -66,9 +70,9 @@ summ.df <- summarySE(gxe.fibersub.SDF, measurevar="value", groupvars=c("Entry","
 #oligosaccharides by mktclass
 p.gxe.SDF <- ggplot(summ.df, aes(x=Origin, y=value, colour=Entry))
 
-p.gxe.SDF <- p.gxe.SDF  + geom_line(aes(group=Entry)) + geom_point() + geom_errorbar(aes(ymin=value-se, ymax=value+se), width=.1)
+p.gxe.SDF <- p.gxe.SDF  + geom_line(aes(group=Entry)) + geom_point() 
 
-p.gxe.SDF <- p.gxe.SDF + theme(axis.title.y=element_blank(),
+p.gxe.SDF <- p.gxe.SDF + theme(axis.title.y=element_text(size=12),
                                        axis.text.y=element_text(family="Helvetica",
                                                                 size=12),
                                        axis.title.x=element_blank(),
@@ -90,9 +94,9 @@ summ.df <- summarySE(gxe.fibersub.Raff, measurevar="value", groupvars=c("Entry",
 #oligosaccharides by mktclass
 p.gxe.Raff <- ggplot(summ.df, aes(x=Origin, y=value, colour=Entry))
 
-p.gxe.Raff <- p.gxe.Raff  + geom_line(aes(group=Entry)) + geom_point() + geom_errorbar(aes(ymin=value-se, ymax=value+se), width=.1)
+p.gxe.Raff <- p.gxe.Raff  + geom_line(aes(group=Entry)) + geom_point() 
 
-p.gxe.Raff <- p.gxe.Raff + theme(axis.title.y=element_blank(),
+p.gxe.Raff <- p.gxe.Raff + theme(axis.title.y=element_text(size=12),
                                  axis.text.y=element_text(family="Helvetica",
                                                           size=12),
                                  axis.title.x=element_blank(),
@@ -101,7 +105,9 @@ p.gxe.Raff <- p.gxe.Raff + theme(axis.title.y=element_blank(),
                                                            size=12),
                                  plot.margin=unit(c(0.5,0.5,0.5,0.5), "cm"), legend.position="none")
 
-
+p.gxe.Raff <- p.gxe.Raff + labs(y=NULL, 
+                              x=NULL, 
+                              title="Raffinose")
 
 
 
@@ -117,7 +123,7 @@ summ.df <- summarySE(gxe.fibersub.Stach, measurevar="value", groupvars=c("Entry"
 #oligosaccharides by mktclass
 p.gxe.Stach <- ggplot(summ.df, aes(x=Origin, y=value, colour=Entry))
 
-p.gxe.Stach <- p.gxe.Stach  + geom_line(aes(group=Entry)) + geom_point() + geom_errorbar(aes(ymin=value-se, ymax=value+se), width=.1)
+p.gxe.Stach <- p.gxe.Stach  + geom_line(aes(group=Entry)) + geom_point() 
 
 p.gxe.Stach <- p.gxe.Stach + theme(axis.title.y=element_blank(),
                                    axis.text.y=element_text(family="Helvetica",
@@ -143,7 +149,7 @@ summ.df <- summarySE(gxe.fibersub.Verb, measurevar="value", groupvars=c("Entry",
 #oligosaccharides by mktclass
 p.gxe.Verb <- ggplot(summ.df, aes(x=Origin, y=value, colour=Entry))
 
-p.gxe.Verb <- p.gxe.Verb  + geom_line(aes(group=Entry)) + geom_point() + geom_errorbar(aes(ymin=value-se, ymax=value+se), width=.1)
+p.gxe.Verb <- p.gxe.Verb  + geom_line(aes(group=Entry)) + geom_point() 
 
 p.gxe.Verb <- p.gxe.Verb + theme(axis.title.y=element_blank(),
                                  axis.text.y=element_text(family="Helvetica",
@@ -171,7 +177,7 @@ summ.df <- summarySE(gxe.fibersub.TOligos, measurevar="value", groupvars=c("Entr
 #oligosaccharides by mktclass
 p.gxe.TOligos <- ggplot(summ.df, aes(x=Origin, y=value, colour=Entry))
 
-p.gxe.TOligos <- p.gxe.TOligos  + geom_line(aes(group=Entry)) + geom_point() + geom_errorbar(aes(ymin=value-se, ymax=value+se), width=.1)
+p.gxe.TOligos <- p.gxe.TOligos  + geom_line(aes(group=Entry)) + geom_point()
 
 p.gxe.TOligos <- p.gxe.TOligos + theme(axis.title.y=element_blank(),
                                        axis.text.y=element_text(family="Helvetica",
